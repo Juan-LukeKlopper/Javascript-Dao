97295b4b7e7e36dc5051d253bd6d04598f3afb87
Juan-luke Klopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-02-10 00:13:29 +0000

    I have added a connect wallet button along with all necessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useMemo, useState } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("Wallet Address: ", address)
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to FreeKnowledgeDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+      <h1>Welcome to the brotherhood.</h1>
+    </div>);
 };
 
 export default App;
