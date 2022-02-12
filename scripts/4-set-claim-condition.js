71d34394596084ddba2d908e3232756667919326
Juan-luke Klopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-02-12 08:39:08 +0000

    added a script to set claiming conditions

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,22 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x75F9FCcb8A035B56b48Ac916aFdE5b1D3E74AA73",
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Successfully set claim condition on bundle drop:", bundleDrop.address);
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})()
