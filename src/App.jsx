c20fef53ca354b97add6524b5295fbb909cb308e
Juan-luke Klopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-02-12 15:19:10 +0000

    Added a button which allows users to mint a membership NFT

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import { useEffect, useMemo, useState } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
+import { ThirdwebSDK } from "@3rdweb/sdk";
 
+const sdk = new ThirdwebSDK("rinkeby");
+
+const bundleDropModule = sdk.getBundleDropModule(
+  "0x75F9FCcb8A035B56b48Ac916aFdE5b1D3E74AA73",
+);
 
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("Wallet Address: ", address)
+
+  const signer = provider ? provider.getSigner() : undefined;
+  const [isClaiming, setIsClaiming] = useState(false);
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(() => {
+    sdk.setProviderOrSigner(signer);
+  }, [signer]);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+  
+    return bundleDropModule
+      .balanceOf(address, "0")
+      .then((balance) => {
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("This user has a membership NFT!")
+        } else {
+          setHasClaimedNFT(false);
+          console.log("This user doesn't have a membership NFT.")
+        }
+      })
+      .catch((error) => {
+        setHasClaimedNFT(false);
+        console.error("failed to nft balance", error);
+      });
+  }, [address]);
+
   if (!address) {
     return (
       <div className="landing">
@@ -16,10 +53,45 @@ const App = () => {
     );
   }
 
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>FreeKnowledgeDAO Member Page</h1>
+        <p>Congratulations on being a member and helping us contribute to the future. </p>
+      </div>
+    );
+  };
+
+  const mintNft = () => {
+    setIsClaiming(true);
+    bundleDropModule
+    .claim("0", 1)
+    .then(() => {
+      setHasClaimedNFT(true);
+      console.log(
+        `🌊 Successfully Minted! Check it our on OpenSea: https://testnets.opensea.io/assets/${bundleDropModule.address.toLowerCase()}/0`
+      );
+    })
+    .catch((err) => {
+      console.error("failed to claim", err);
+    })
+    .finally(() => {
+      setIsClaiming(false);
+    });
+  }
+
   return (
-    <div className="landing">
-      <h1>Welcome to the FreeKnowledgeDAO.</h1>
-    </div>);
+    <div className="mint-nft">
+      <h1>Mint your free DAO Membership NFT</h1>
+      <p>You will just have to pay the gasfee to write your details to the blockchain. :)</p>
+      <button
+        disabled={isClaiming}
+        onClick={() => mintNft()}
+      >
+        {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+      </button>
+    </div>
+  );
 };
 
 export default App;
